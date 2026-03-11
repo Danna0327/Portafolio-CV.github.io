2eb2b77af974c45c8fa1366c0f564b605f0deee1
Danna0327 <155197785+Danna0327@users.noreply.github.com>
2026-03-10 21:34:16 -0500

    Update Proyectos.js

--- src/Componentes/Proyectos.js
+++ src/Componentes/Proyectos.js
@@ -11,7 +11,7 @@ const proyectos = [
     descripcion: 'Software educativo diseñado para facilitar el aprendizaje de las matemáticas de forma interactiva y dinámica.',
     imagen: mathspin,
     tags: ['JavaScript', 'Educación'],
-    github: 'https://github.com/Danna0327',
+    github: 'https://github.com/Danna0327/mathspin',
     demo: '#',
   },
   {
@@ -20,7 +20,7 @@ const proyectos = [
     descripcion: 'Sistema de gestión web diseñado para el negocio familiar de abarrotes.',
     imagen: proyectoWeb,
     tags: ['React', 'TIC'],
-    github: 'https://github.com/Danna0327',
+    github: 'https://github.com/Danna0327/abastosgest',
     demo: '#',
   },
   {
@@ -29,7 +29,7 @@ const proyectos = [
     descripcion: 'Sitio web personal desarrollado con React para mostrar mi trayectoria, habilidades y proyectos en el campo de la computación.',
     imagen: portafolio,
     tags: ['React', 'CSS'],
-    github: 'https://github.com/Danna0327',
+    github: 'https://github.com/Danna0327/Portafolio-CV.github.io',
     demo: '#',
   },
 ];
